9dbe413f301ccea595887113311518474675523a
squireblank <squireblank@gmail.com>
2022-12-25 14:03:54 +0600

    html boilerplate

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import "./style.css";
+import getweather from "./weather";
+
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// getweather()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err.message));
